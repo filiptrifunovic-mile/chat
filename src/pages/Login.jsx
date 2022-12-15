efd1b9c144f75f5837833a38978d33f1777f8c8b
Filip <filip.trifunovic23@gmail.com>
2022-12-15 23:27:39 +0100

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Add from "../img/addAvatar.png";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat App</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+
+          <button>Sign in</button>
+        </form>
+        <p>You dont have an account? Register here</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
