12bea7b69c95781db0cca1ff192dae54a23f95a5
Filip <filip.trifunovic23@gmail.com>
2022-12-17 22:18:07 +0100

    creating context

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Home from "./pages/Home";
 import "./style.scss";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
+
   return (
     <BrowserRouter>
       <Routes>
