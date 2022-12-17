168c407c590d48bfd8c36831e2642faba0502fd6
Filip <filip.trifunovic23@gmail.com>
2022-12-17 15:26:52 +0100

    input message style

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Img from "../img/1.jpg";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src={Img} alt="imageAvatar" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src={Img} alt="avatarImage" />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
